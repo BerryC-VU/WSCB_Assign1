51f56290be176cd1e4caba28a6158ab302aabdc5
BerryC-VU <2672919@student.vu.nl>
2023-04-05 15:11:41 +0200

    Check validity of URL

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # reference: https://www.restapitutorial.com/lessons/httpmethods.html
 from flask import Flask, request
 import base62
+import validators
 
 app = Flask(__name__)
 
@@ -11,18 +12,24 @@ url_dict = {}
 @app.route('/', methods=['POST'])
 def post_url():
     # get url
-    url = request.args
+    url = request.args['url']
     # check whether url already exists
     values = list(url_dict.values())
     if url in values:
         keys = list(url_dict.keys())
         url_id = keys[values.index(url)]
-        return ("The url already exists, the shortened id is " + url_id), 400
-    # encode to get identifier
-    url_id = base62.base62_encode(len(url_dict))
-    # store in the dict
-    url_dict[url_id] = url
-    return url_id, 201
+        return ("The url already exists, the shortened identifier is " + url_id), 400
+    else:
+        # if the url is valid, then return id and add it to the dict
+        if validators.url(url):
+            # encode to get identifier
+            url_id = base62.base62_encode(len(url_dict))
+            # store in the dict
+            url_dict[url_id] = url
+            return url_id, 201
+        # if the url is not valid
+        else:
+            return "400 Error: The URL is not valid", 400
 
 @app.route('/', methods=['GET'])
 def get():
@@ -56,11 +63,15 @@ def delete_url(url_id):
 @app.route('/<string:url_id>', methods=["PUT"])
 def put_url(url_id):
     if url_id in url_dict:
-        # get url
-        new_url = request.args 
-        # update url to the given identifier
-        url_dict[url_id] = new_url
-        return "The URL has been updated", 200
+        # get new url
+        new_url = request.args['url'] 
+        # check validity
+        if validators.url(new_url):
+            # update url to the given identifier
+            url_dict[url_id] = new_url
+            return "The URL has been updated", 200
+        else:
+            return "400 Error: The URL is not valid", 400
     else:
         return "404 Error: The identifier does not exist", 404
 
