3565f13f6dd2c4e940bfb00bbffdaded15c2e191
Weiqiang Guo <794215312@qq.com>
2023-04-05 21:17:36 +0200

    add lock and finish id generating

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import re
 import requests
+import threading
+import heapq
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse, inputs
+
+lock = threading.Lock()
 
 # https://stackoverflow.com/questions/8498371/curl-get-and-x-get
 # input format: curl url -d "url=" -X GET/PUT/POST/DELETE -v
@@ -12,7 +16,12 @@ parser = reqparse.RequestParser()
 # https://stackoverflow.com/questions/72893180/flask-restful-error-request-content-type-was-not-application-json
 parser.add_argument('url', type=str, location='form', help='input example: http(s)://www.google.com/')
 
-mappings = {'2': 'http://www.google.com'}
+mappings = {'0': 'http://www.google.com'}
+
+end = 3
+id_pool = list(range(1, end))
+
+heapq.heapify(id_pool)
 
 def is_valid(url):
     def check_format(url):
@@ -39,8 +48,12 @@ def is_valid(url):
 
 
 def generate_id(url):
-    return 1
-
+    global id_pool
+    global end
+    if len(id_pool) == 0:
+        id_pool = list(range(end, end + 5))
+        end = end + 5
+    return str(heapq.heappop(id_pool))
 
 class AccessWithID(Resource):
     def get(self, id):
@@ -56,23 +69,26 @@ class AccessWithID(Resource):
         # 1. 200
         # 2. 400, "error" (400 Bad Request)
         # 3. 404
-        if id not in mappings:
-            return 404
-        args = parser.parse_args()
-        url = args['url']
-        if not is_valid(url):
-            return 400
-        else:
-            mappings[id] = url
-            return url, 200
+        with lock:
+            if id not in mappings:
+                return 404
+            args = parser.parse_args()
+            url = args['url']
+            if not is_valid(url):
+                return 400
+            else:
+                mappings[id] = url
+                return url, 200
     
     def delete(self, id):
         # 1. 204 (204 No Content)
         # 2. 404
-        if id not in mappings:
-            return 404
-        del mappings[id]
-        return 204
+        with lock:
+            if id not in mappings:
+                return 404
+            del mappings[id]
+            heapq.heappush(id_pool, int(id))
+            return 204
 
 
 class AccessWithoutID(Resource):
@@ -83,14 +99,20 @@ class AccessWithoutID(Resource):
     def post(self):
         # 1. 201, id (201 Created)
         # 2. 400, "error"
-        args = parser.parse_args()
-        url = args['url'] 
-        if is_valid(url):
-            id = generate_id(url=url)
-            mappings[id] = url
-            return id, 201
-        else:
-            return "error", 400
+        with lock:
+            args = parser.parse_args()
+            url = args['url'] 
+            if is_valid(url):
+                if url in mappings.values():
+                    for k, v in mappings.items():
+                        if v == url:
+                            return k, 201  
+                else:
+                    id = generate_id(url=url)
+                    mappings[id] = url
+                    return id, 201
+            else:
+                return "error", 400 
     
     def delete(self):
         # 1. 404
@@ -101,4 +123,5 @@ api.add_resource(AccessWithID, '/<string:id>')
 
 
 if __name__ == '__main__':
+    # https://github.com/pallets/flask/blob/main/src/flask/app.py#L873
     app.run(debug=True)
